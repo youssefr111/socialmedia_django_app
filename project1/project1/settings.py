6f709ed35c39b9459298c39127e397c67e76331f
youssefr111 <youssefrua11@gmail.com>
2025-10-24 20:42:26 +0300

    Initial deploy

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0etndo^_0^bt(5i3$(e!1ipt@ccdn)h6cr#_&ez(4xf+6945db'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
